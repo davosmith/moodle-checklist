b00ce6993dda0c0e4b8eb80acd35a04fcd13f64c
Davo Smith <git@davosmith.co.uk>
2010-02-27 11:40:14 +0000

    Added German language support - thanks to Joachim Vogelgesang

---
+++ lang/de_utf8/checklist.php
@@ -0,0 +1,75 @@
+<?php
+
+$string['additem'] = 'Hinzufügen';
+$string['additemalt'] = 'Ein neues Element in die Liste einfügen';
+$string['additemhere'] = 'Ein neues Element nach diesem einfügen';
+$string['addownitems'] = 'Fügen Sie Ihre eigenen Elemente ein';
+$string['addownitems-stop'] = 'Beenden Sie das Einfügen der eigenen Elemente';
+$string['canceledititem'] = 'Streiche';
+
+$string['checkeditemsdeleted'] = 'Ausgewählte Elemente gelöscht';
+
+$string['checklist'] = 'Kontrollliste';
+
+$string['checklist:edit'] = 'Erstellen und Bearbeiten von Checklisten';
+$string['checklist:preview'] = 'Vorschau Fortschrittliste';
+$string['checklist:updateother'] = 'Aktualisieren der Teilnehmerkennzeichnung der Kontrollisten';
+$string['checklist:updateown'] = 'Aktualisieren Ihrer Kontrolllistenkennzeichnung';
+$string['checklist:viewreports'] = 'Teilnehmer Fortschritt anschauen';
+
+$string['checklistintro'] = 'Einführung';
+$string['checklistsettings'] = 'Einstellungen';
+
+$string['checks'] = 'Kennzeichnungsprüfung';
+
+$string['deleteitem'] = 'Dieses Element löschen';
+
+$string['edit'] = 'Kontrollliste bearbeiten';
+$string['edititem'] = 'Dieses Element bearbeiten';
+
+$string['guestsno'] = 'Sie haben nicht die Berechtigung diese Kontrollliste zu sehen';
+
+$string['indentitem'] = 'Eingerücktes Element';
+$string['itemcomplete'] = 'Fertiggestellt';
+$string['items'] = 'Kontrollistenelement';
+
+$string['modulename'] = 'Kontrolliste';
+$string['modulenameplural'] = 'Kontrolllisten';
+
+$string['moveitemdown'] = 'Element nach unten bewegen';
+$string['moveitemup'] = 'Element nach oben bewegen';
+
+$string['noitems'] = 'Keine Elemente in der Kontrollliste';
+
+$string['optionalitem'] = 'Dieses Element ist optional';
+$string['optionalhide'] = 'Optionale Elemente verbergen';
+$string['optionalshow'] = 'Optionale Elemente zeigen';
+
+$string['percentcomplete'] = 'Notwendiges Element';
+$string['percentcompleteall'] = 'Alle Elemente';
+$string['preview'] = 'Vorschau';
+
+$string['report'] = 'Fortschritt anschauen';
+$string['reporttablesummary'] = 'Die Tabelle zeigt alle Elemente auf der Kontrollliste an, die jeder einzelne Teilnehmer abgeschlossen hat';
+
+$string['requireditem'] = 'Dieser Posten ist erforderlich - es muss alles ausgefüllt werden';
+
+$string['resetchecklistprogress'] = 'Kontrolllistenfortschritt und Benutzerelemente zurücksetzen';
+
+$string['savechecks'] = 'Speichern';
+
+$string['teacheredit'] = 'Auswirkung der Kontrollmarkierungen durch Dozentenbearbeitung ';
+$string['teachernoteditcheck'] = 'Nicht erlaubt';
+$string['teacheroverwritecheck'] = 'Nutzer überschreiben';
+$string['teacheralongsidecheck'] = 'Neben Benutzer';
+
+$string['theme'] = 'Darstellung, die bei der Kontrollliste angezeigt wird';
+
+$string['unindentitem'] = 'Element ohne Einrückung';
+$string['updateitem'] = 'Aktualisieren';
+$string['useritemsallowed'] = 'Teilnehmer kann seine eigenen Elemente hinzufügen';
+$string['useritemsdeleted'] = 'Teilnehmer Elemente gelöscht';
+
+$string['view'] = 'Kontrollliste anschauen';
+
+?>
