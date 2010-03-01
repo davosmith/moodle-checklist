7bcc6ba6729ee113795e7feecb739f58d3108e08
Davo Smith <git@davosmith.co.uk>
2010-03-01 15:06:56 +0000

    Added Japanese language file

---
+++ lang/ja_utf8/checklist.php
@@ -0,0 +1,89 @@
+<?php
+
+$string['additem'] = '追加';
+$string['additemalt'] = 'リストに新しい項目を追加';
+$string['additemhere'] = '下に追加';
+$string['addownitems'] = '自分の項目を追加';
+$string['addownitems-stop'] = '項目の追加を中止';
+$string['canceledititem'] = 'キャンセル';
+
+$string['checkeditemsdeleted'] = 'チェックした項目を削除';
+
+$string['checklist'] = 'チェックリスト';
+
+$string['checklist:edit'] = '作成と編集';
+$string['checklist:preview'] = 'プレビュー';
+$string['checklist:updateother'] = '学生の完了チェックを更新';
+$string['checklist:updateown'] = '自分の完了チェックを更新';
+$string['checklist:viewreports'] = '学生の進捗を見る';
+
+$string['checklistfor'] = 'チェックリスト for ';
+
+$string['checklistintro'] = '説明文';
+$string['checklistsettings'] = '設定';
+
+$string['checks'] = '完了にする';
+
+$string['deleteitem'] = '削除';
+
+$string['edit'] = '編集';
+$string['edititem'] = '編集';
+
+$string['guestsno'] = 'このチェックリストを閲覧する権限がありません。';
+
+$string['indentitem'] = '右へインデント';
+$string['itemcomplete'] = '完了';
+$string['items'] = '項目';
+
+$string['modulename'] = 'チェックリスト';
+$string['modulenameplural'] = 'チェックリスト';
+
+$string['moveitemdown'] = '下へ移動';
+$string['moveitemup'] = '上へ移動';
+
+$string['noitems'] = '項目がありません';
+
+$string['optionalitem'] = 'この項目は必須ではありません。';
+$string['optionalhide'] = '必須ではない項目を隠す';
+$string['optionalshow'] = '必須ではない項目を表示';
+
+$string['percentcomplete'] = '必須項目';
+$string['percentcompleteall'] = 'すべての項目';
+$string['preview'] = 'プレビュー';
+
+$string['report'] = '進捗確認';
+$string['reporttablesummary'] = 'サマリーレポート';
+
+$string['requireditem'] = 'この項目は必須です。必ず完了させる必要があります。';
+
+$string['resetchecklistprogress'] = '進捗とユーザーの項目をリセットする。';
+
+$string['savechecks'] = '保存';
+
+
+$string['teacheredit'] = '編集可能なロール';
+
+$string['teachermarkundecided'] = '教師がまだ完了にしていません。';
+$string['teachermarkyes'] = '教師が完了にしました。';
+$string['teachermarkno'] = '教師がまだ完了していないと言っています。';
+
+$string['teachernoteditcheck'] = '学生のみ';
+$string['teacheroverwritecheck'] = '教師のみ';
+$string['teacheralongsidecheck'] = '学生と教師';
+
+$string['theme'] = 'テーマ';
+
+$string['unindentitem'] = '左へインデント';
+$string['updateitem'] = '更新';
+$string['useritemsallowed'] = 'ユーザーが独自の項目を追加';
+$string['useritemsdeleted'] = 'ユーザー項目は削除されました。';
+
+$string['view'] = '表示';
+$string['viewall'] = 'すべての進捗を表示';
+$string['viewallcancel'] = 'キャンセル';
+$string['viewallsave'] = '保存';
+
+$string['viewsinglereport'] = 'このユーザーの進捗を表示';
+$string['viewsingleupdate'] = 'このユーザーの進捗を更新';
+
+?>
