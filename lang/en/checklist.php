9226bac3f4c4f40f76daea813886289ce38d05b2
Davo Smith <git@davosmith.co.uk>
2010-05-06 00:05:27 +0100

    Moved language files

---
+++ lang/en/checklist.php
@@ -0,0 +1,105 @@
+<?php
+
+$string['addcomments'] = 'Add comments';
+
+$string['additem'] = 'Add';
+$string['additemalt'] = 'Add a new item to the list';
+$string['additemhere'] = 'Insert new item after this one';
+$string['addownitems'] = 'Add your own items';
+$string['addownitems-stop'] = 'Stop adding your own items';
+$string['canceledititem'] = 'Cancel';
+
+$string['calendardescription'] = 'This event was added by the checklist: $a';
+
+$string['checkeditemsdeleted'] = 'Checked items deleted';
+
+$string['checklist'] = 'checklist';
+
+$string['checklist:edit'] = 'Create and edit checklists';
+$string['checklist:preview'] = 'Preview a checklist';
+$string['checklist:updateother'] = 'Update students\' checklist marks';
+$string['checklist:updateown'] = 'Update your checklist marks';
+$string['checklist:viewreports'] = 'View students\' progress';
+
+$string['checklistfor'] = 'Checklist for';
+
+$string['checklistintro'] = 'Introduction';
+$string['checklistsettings'] = 'Settings';
+
+$string['checks'] = 'Check marks';
+$string['comments'] = 'Comments';
+
+$string['confirmdeleteitem'] = 'Are you sure you want to permanently delete this checklist item?';
+
+$string['deleteitem'] = 'Delete this item';
+
+$string['duedatesoncalendar'] = 'Add due dates to calendar';
+
+$string['edit'] = 'Edit checklist';
+$string['editdatesstart'] = 'Edit dates';
+$string['editdatesstop'] = 'Stop editing dates';
+$string['edititem'] = 'Edit this item';
+
+$string['guestsno'] = 'You do not have permission to view this checklist';
+
+$string['indentitem'] = 'Indent item';
+$string['itemcomplete'] = 'Completed';
+$string['items'] = 'Checklist items';
+
+$string['modulename'] = 'Checklist';
+$string['modulenameplural'] = 'Checklists';
+
+$string['moveitemdown'] = 'Move item down';
+$string['moveitemup'] = 'Move item up';
+
+$string['noitems'] = 'No items in the checklist';
+
+$string['optionalitem'] = 'This item is optional';
+$string['optionalhide'] = 'Hide optional items';
+$string['optionalshow'] = 'Show optional items';
+
+$string['percentcomplete'] = 'Required items';
+$string['percentcompleteall'] = 'All items';
+$string['preview'] = 'Preview';
+$string['progress'] = 'Progress';
+
+$string['report'] = 'View Progress';
+$string['reporttablesummary'] = 'Table showing the items on the checklist that each student has completed';
+
+$string['requireditem'] = 'This item is required - it must be completed';
+
+$string['resetchecklistprogress'] = 'Reset checklist progress and user items';
+
+$string['savechecks'] = 'Save';
+
+$string['showfulldetails'] = 'Show full details';
+$string['showprogressbars'] = 'Show progress bars';
+
+$string['teachercomments'] = 'Teachers can add comments';
+
+$string['teacheredit'] = 'Updates by';
+
+$string['teachermarkundecided'] = 'Teacher has not yet marked this';
+$string['teachermarkyes'] = 'Teacher states that you have completed this';
+$string['teachermarkno'] = 'Teacher states that you have NOT completed this';
+
+$string['teachernoteditcheck'] = 'Student only';
+$string['teacheroverwritecheck'] = 'Teacher only';
+$string['teacheralongsidecheck'] = 'Student and teacher';
+
+$string['theme'] = 'Checklist display theme';
+
+$string['unindentitem'] = 'Unindent item';
+$string['updateitem'] = 'Update';
+$string['useritemsallowed'] = 'User can add their own items';
+$string['useritemsdeleted'] = 'User items deleted';
+
+$string['view'] = 'View checklist';
+$string['viewall'] = 'View all progress';
+$string['viewallcancel'] = 'Cancel';
+$string['viewallsave'] = 'Save';
+
+$string['viewsinglereport'] = 'View progress for this user';
+$string['viewsingleupdate'] = 'Update progress for this user';
+
+?>
---
+++ lang/es/checklist.php
@@ -0,0 +1,82 @@
+<?php
+
+$string['additem'] = 'Añadir';
+$string['additemalt'] = 'Añade pieza nueva a la lista';
+$string['additemhere'] = 'Ponga pieza nueva después de esta pieza';
+$string['addownitems'] = 'Añade tus propias piezas';
+$string['addownitems-stop'] = 'Terminar el proceso de añadir tus propias piezas';
+$string['canceledititem'] = 'Cancelar';
+
+$string['checkeditemsdeleted'] = 'Las piezas sellecionadas han sido borrados';
+
+$string['checklist'] = 'Lista de control';
+
+$string['checklist:edit'] = 'Crear y editar listas de control';
+$string['checklist:preview'] = 'Prevista de una lista de control';
+$string['checklist:updateother'] = 'Actualizar notas para la lista del estudiante';
+$string['checklist:updateown'] = 'Actualizar notas para su propia lista';
+$string['checklist:viewreports'] = 'Ver progreso de los estudiantes';
+$string['checklistfor'] = 'Lista de control de';
+$string['checklistintro'] = 'Introducción';
+$string['checklistsettings'] = 'Ajustes';
+
+$string['checks'] = 'Marcas';
+
+$string['deleteitem'] = 'Eliminar pieza';
+
+$string['edit'] = 'Editar lista de control';
+$string['edititem'] = 'Editar pieza';
+
+$string['guestsno'] = 'No está autorizado para revisar esta lista de control';
+
+$string['indentitem'] = 'Sangrar pieza';
+$string['itemcomplete'] = 'Hecho';
+$string['items'] = 'Piezas';
+
+$string['modulename'] = 'Lista de control';
+$string['modulenameplural'] = 'Listas de control';
+
+$string['moveitemdown'] = 'Bajar pieza';
+$string['moveitemup'] = 'Subir pieza';
+
+$string['noitems'] = 'No hay piezas en la lista de control';
+
+$string['optionalitem'] = 'Pieza opcional';
+$string['optionalhide'] = 'Esconde piezas opcionales';
+$string['optionalshow'] = 'Muestra piezas opcionales';
+
+$string['percentcomplete'] = 'Piezas necesarias';
+$string['percentcompleteall'] = 'Todas las piezas';
+$string['preview'] = 'Prevista';
+
+$string['report'] = 'Ver progreso';
+$string['reporttablesummary'] = 'Tabla indicando piezas que cada alumno ha hecho';
+
+$string['requireditem'] = 'Pieza necesaria - hay que hacerla';
+
+$string['resetchecklistprogress'] = 'Reiniciar progreso y piezas del usuario';
+
+$string['savechecks'] = 'Guardar';
+
+$string['teacheredit'] = 'Resulta de la modificación de la marca por profesor(a)';
+$string['teachernoteditcheck'] = 'No está permitido';
+$string['teacheroverwritecheck'] = 'Anular ajustes del usuario';
+$string['teacheralongsidecheck'] = 'Con el usuario';
+
+$string['theme'] = 'Tema visual para lista de control';
+
+$string['unindentitem'] = 'Desangrar pieza';
+$string['updateitem'] = 'Actualizar';
+$string['useritemsallowed'] = 'Ususario puede añadir sus propias piezas';
+$string['useritemsdeleted'] = 'Piezas del usuarios están borrados';
+
+$string['view'] = 'Ver lista de control';
+
+$string['viewall'] = 'Ver todo';
+$string['viewallcancel'] = 'Cancelar';
+$string['viewallsave'] = 'Guardar';
+
+$string['viewsinglereport'] = 'Ver progreso de este usuario';
+$string['viewsingleupdate'] = 'Actualizar progreso de este usuario';
+
+?>
