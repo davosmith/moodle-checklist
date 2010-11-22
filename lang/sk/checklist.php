832d1bd6254f012ad017e9da6ee54827e93761d4
Davo Smith <git@davosmith.co.uk>
2010-11-22 21:13:59 +0000

    Added Slovak translation from Peter Mocsi

---
+++ lang/sk/checklist.php
@@ -0,0 +1,113 @@
+<?php
+
+$string['addcomments'] = 'Pridaj poznámky';
+
+$string['additem'] = 'Pridať';
+$string['additemalt'] = 'Pridaj novú položku do zoznamu';
+$string['additemhere'] = 'Vložiť novú položku za touto ';
+$string['addownitems'] = 'Pridaj vlastnú položku';
+$string['addownitems-stop'] = 'Zastav pridávanie položiek';
+
+$string['allowmodulelinks'] = 'Povoliť modul odkazy';
+
+$string['canceledititem'] = 'Zrušiť';
+
+$string['calendardescription'] = 'Táto udalosť bola pridaná z kontrolného zoznamu: $a';
+
+$string['changetextcolour'] = 'Ďalšia farba textu';
+
+$string['checkeditemsdeleted'] = 'Kontrolovať odstránenú položky';
+
+$string['checklist'] = 'Kontrolný zoznam';
+
+$string['checklist:edit'] = 'Vytvoriť a upravovať zoznamy';
+$string['checklist:preview'] = 'Náhľad zoznamu';
+$string['checklist:updateother'] = 'Aktualizácia študentov\' zoznam značiek';
+$string['checklist:updateown'] = 'Aktualizujte svoj zoznam zaznačení';
+$string['checklist:viewreports'] = 'Zobraziť študentov\' pokrok';
+
+$string['checklistfor'] = 'Zoznam pre';
+
+$string['checklistintro'] = 'Úvod';
+$string['checklistsettings'] = 'Nastavenia';
+
+$string['checks'] = 'Zaškrtnúť položku ';
+$string['comments'] = 'Poznámky';
+
+$string['configallowmodulelinks'] = 'Povoliť prepojenie zoznam položiek na inýmé modulmi (môže spomaliť editáciu zoznamy)';
+$string['confirmdeleteitem'] = 'Ste si istý ?';
+
+$string['deleteitem'] = 'Zmazat túto položku';
+
+$string['duedatesoncalendar'] = 'Pridať splatnosť do kalendára';
+
+$string['edit'] = 'Upraviť zoznam';
+$string['editdatesstart'] = 'Upraviť dátum';
+$string['editdatesstop'] = 'Ukončiť editovanie dátumu';
+$string['edititem'] = 'Editovať túto položku';
+
+$string['guestsno'] = 'Nemáte oprávnenie na prezeranie tohto zoznamu';
+
+$string['headingitem'] = 'Táto položka je len hlavička - to nebude mať políčko vedľa';
+
+$string['indentitem'] = 'Zarážka bodu';
+$string['itemcomplete'] = 'Dokončiť';
+$string['items'] = 'Položky kontrolného zoznamu';
+
+$string['modulename'] = 'Kontrolný zoznam';
+$string['modulenameplural'] = 'Kontrolný zoznamy';
+
+$string['moveitemdown'] = 'Posunúť položku dole';
+$string['moveitemup'] = 'Posunúť položku hore';
+
+$string['noitems'] = 'Žiadne položky v zozname';
+
+$string['optionalitem'] = 'Táto položka je nepovinná';
+$string['optionalhide'] = 'Skryť nepovinné položky';
+$string['optionalshow'] = 'Zobraziť nepovinnú položky';
+
+$string['percentcomplete'] = 'Povinnú položky';
+$string['percentcompleteall'] = 'Všetky položky';
+$string['preview'] = 'Prezrieť';
+$string['progress'] = 'Výkon';
+
+$string['report'] = 'Zobraziť výkon';
+$string['reporttablesummary'] = 'Tabuľka zobrazuje položky, ktoré každý splnil';
+
+$string['requireditem'] = 'Táto položka je vyžadovaná';
+
+$string['resetchecklistprogress'] = 'Nuluj zoznam pokroku a užívateľské položky';
+
+$string['savechecks'] = 'Uložiť';
+
+$string['showfulldetails'] = 'Ukázať všetky podrobnosti';
+$string['showprogressbars'] = 'Zobraziť pokrok graficky';
+
+$string['teachercomments'] = 'Učitelia môžu pridávať komentáre';
+
+$string['teacheredit'] = 'Aktualizoval';
+
+$string['teachermarkundecided'] = 'Učiteľ teraz nemôže označovať';
+$string['teachermarkyes'] = 'Učiteľ hovorí, že ste absolvovali tieto úlohy';
+$string['teachermarkno'] = 'Učiteľ hovorí, že ste nedokončil tieto úlohy';
+
+$string['teachernoteditcheck'] = 'Iba študenti';
+$string['teacheroverwritecheck'] = 'Iba učitelia';
+$string['teacheralongsidecheck'] = 'Učitelia a študenti';
+
+$string['theme'] = 'Téma zobrazenia kontrolného zoznamu';
+
+$string['unindentitem'] = 'Zväčšiť odsadenie';
+$string['updateitem'] = 'Aktualizuj';
+$string['useritemsallowed'] = 'Užívateľ môže pridávať vlastné položky';
+$string['useritemsdeleted'] = 'Užívateľské položky vymazané';
+
+$string['view'] = 'Zobraziť zoznam';
+$string['viewall'] = 'Zobraziť pokrok všetky';
+$string['viewallcancel'] = 'Zrušiť';
+$string['viewallsave'] = 'Uložiť';
+
+$string['viewsinglereport'] = 'Zobraziť pokrok tohto užívateľa';
+$string['viewsingleupdate'] = 'Aktualizácia pokroku tohto užívateľa';
+
+?>
