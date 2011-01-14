645978648670e2f1482c20ceab28701d45e71e59
Davo Smith <git@davosmith.co.uk>
2011-01-14 19:30:10 +0000

    Removed the unnecessary 'forceupdate.php' file

--- forceupdate.php
+++
@@ -1,6 +0,0 @@
-<html><head><title>Updating checklist</title></head>
-<body>
-<h1>Please do not use this file</h1>
-<p>Instead, just update one of the completion grades and save it</p>
-</body>
-</html>
