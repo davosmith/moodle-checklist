624f46cbcf71a32bdfa3ac98fe179d72df32ad32
Davo Smith <git@davosmith.co.uk>
2011-02-13 22:15:33 +0000

    Removed the autoupdate global setting

--- mod_form.php
+++ mod_form.php
@@ -87,15 +87,13 @@ class mod_checklist_mod_form extends moodleform_mod {
         $mform->setDefault('autopopulate', 0);
         $mform->setHelpButton('autopopulate', array('autopopulate', get_string('autopopulate','checklist'), 'checklist'));
 
-        if ($CFG->checklist_auto_update) {
-            $autoupdate_options = array( CHECKLIST_AUTOUPDATE_NO => get_string('no'), 
-                                         CHECKLIST_AUTOUPDATE_YES => get_string('yesnooverride', 'checklist'), 
-                                         CHECKLIST_AUTOUPDATE_YES_OVERRIDE => get_string('yesoverride', 'checklist'));
-            $mform->addElement('select', 'autoupdate', get_string('autoupdate', 'checklist'), $autoupdate_options);
-            $mform->setDefault('autoupdate', 1);
-            $mform->disabledIf('autoupdate', 'autopopulate', 'eq', 0);
-            $mform->setHelpButton('autoupdate', array('autoupdate', get_string('autoupdate','checklist'), 'checklist'));
-        }
+        $autoupdate_options = array( CHECKLIST_AUTOUPDATE_NO => get_string('no'), 
+                                     CHECKLIST_AUTOUPDATE_YES => get_string('yesnooverride', 'checklist'), 
+                                     CHECKLIST_AUTOUPDATE_YES_OVERRIDE => get_string('yesoverride', 'checklist'));
+        $mform->addElement('select', 'autoupdate', get_string('autoupdate', 'checklist'), $autoupdate_options);
+        $mform->setDefault('autoupdate', 1);
+        $mform->disabledIf('autoupdate', 'autopopulate', 'eq', 0);
+        $mform->setHelpButton('autoupdate', array('autoupdate', get_string('autoupdate','checklist'), 'checklist'));
         
 //-------------------------------------------------------------------------------
         // add standard elements, common to all modules
--- settings.php
+++ settings.php
@@ -5,7 +5,7 @@
 /*$settings->add(new admin_setting_configcheckbox('checklist_module_links', get_string('allowmodulelinks', 'checklist'),
   get_string('configallowmodulelinks', 'checklist'), 1));*/
                    
-$settings->add(new admin_setting_configcheckbox('checklist_auto_update', get_string('checklistautoupdate', 'checklist'),
-                   get_string('configchecklistautoupdate', 'checklist'), 0));
+/*$settings->add(new admin_setting_configcheckbox('checklist_auto_update', get_string('checklistautoupdate', 'checklist'),
+  get_string('configchecklistautoupdate', 'checklist'), 0));*/
 
 ?>
