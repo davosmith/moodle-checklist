2cb4cc171150cd7de200c947a595b3799c5986ba
Davo Smith <git@davosmith.co.uk>
2011-03-03 20:00:27 +0000

    Updated Slovak language - thanks to Peter Mocsi

--- blocks/checklist/lang/sk_utf8/block_checklist.php
+++ blocks/checklist/lang/sk_utf8/block_checklist.php
@@ -4,7 +4,7 @@ $string['checklist'] = 'Kontrolný zoznam';
 $string['choosechecklist'] = 'Vyberte zoznam';
 $string['choosegroup'] = 'Vyberte skupinu';
 $string['nochecklist'] = 'Prosím upravte tento blok na zobrazenie kontrolného zoznamu';
-$string['nochecklistplugin'] = 'Je potrebné nainštalovať najnovšiu verziu zoznamu plugin pre tento blok do práce';
+$string['nochecklistplugin'] = 'Pre prácu s týmto blokom je potrebné nainštalovať najnovšiu verziu pluginu';
 $string['nousers'] = 'Žiadni užívatelia';
 
 ?>
--- mod/checklist/lang/sk_utf8/checklist.php
+++ mod/checklist/lang/sk_utf8/checklist.php
@@ -3,12 +3,17 @@
 $string['addcomments'] = 'Pridaj poznámky';
 
 $string['additem'] = 'Pridať';
-$string['additemalt'] = 'Pridaj novú položku do zoznamu';
+$string['additemalt'] = 'Pridať novú položku do zoznamu';
 $string['additemhere'] = 'Vložiť novú položku za touto ';
 $string['addownitems'] = 'Pridaj vlastnú položku';
 $string['addownitems-stop'] = 'Zastav pridávanie položiek';
 
-$string['allowmodulelinks'] = 'Povoliť modul odkazy';
+$string['allowmodulelinks'] = 'Povoliť odkazy na modul';
+
+$string['anygrade'] = 'Akúkoľvek';
+
+$string['autopopulate'] = 'Zobraziť v zozname aj moduly kurzu';
+$string['autoupdate'] = 'Zaškrtnúť, ak je modul kompletný';
 
 $string['canceledititem'] = 'Zrušiť';
 
@@ -16,7 +21,7 @@ $string['calendardescription'] = 'Táto udalosť bola pridaná z kontrolného zo
 
 $string['changetextcolour'] = 'Ďalšia farba textu';
 
-$string['checkeditemsdeleted'] = 'Kontrolovať odstránenú položky';
+$string['checkeditemsdeleted'] = 'Zaškrtnuté položky odstránené';
 
 $string['checklist'] = 'Kontrolný zoznam';
 
@@ -26,18 +31,21 @@ $string['checklist:updateother'] = 'Aktualizácia študentov\' zoznam značiek';
 $string['checklist:updateown'] = 'Aktualizujte svoj zoznam zaznačení';
 $string['checklist:viewreports'] = 'Zobraziť študentov\' pokrok';
 
+$string['checklistautoupdate'] = 'Povoliť automatickú aktualizáciu zoznamov';
+
 $string['checklistfor'] = 'Zoznam pre';
 
 $string['checklistintro'] = 'Úvod';
 $string['checklistsettings'] = 'Nastavenia';
 
-$string['checks'] = 'Zaškrtnúť položku ';
+$string['checks'] = 'Zaškrtnúť položku';
 $string['comments'] = 'Poznámky';
-
-$string['configallowmodulelinks'] = 'Povoliť prepojenie zoznam položiek na inýmé modulmi (môže spomaliť editáciu zoznamy)';
+$string['completiongradehelp'] = 'Známka za dokončenie je surové skóre, nie percento';
+$string['configallowmodulelinks'] = 'Povoliť prepojenie položky zoznamu na inýmé modulmi (môže spomaliť editáciu zoznamy)';
+$string['configchecklistautoupdate'] = 'Pred povolením tejto funkcie musíte urobiť niekoľko zmien v kóde jadra Moodle, podrobnosti nájdete v mod/checklist/README.txt ';
 $string['confirmdeleteitem'] = 'Ste si istý ?';
 
-$string['deleteitem'] = 'Zmazat túto položku';
+$string['deleteitem'] = 'Zmazať túto položku';
 
 $string['duedatesoncalendar'] = 'Pridať splatnosť do kalendára';
 
@@ -46,16 +54,21 @@ $string['editdatesstart'] = 'Upraviť dátum';
 $string['editdatesstop'] = 'Ukončiť editovanie dátumu';
 $string['edititem'] = 'Editovať túto položku';
 
+$string['forceupdate'] = 'aktualizovať zaškrtnurie pre všetky automatické položky';
+
+$string['gradetocomplete'] = 'Stupne na dokončenie:';
 $string['guestsno'] = 'Nemáte oprávnenie na prezeranie tohto zoznamu';
 
-$string['headingitem'] = 'Táto položka je len hlavička - to nebude mať políčko vedľa';
+$string['headingitem'] = 'Táto položka je len hlavička - to nebude mať začiarkavacie políčko vedľa';
 
 $string['indentitem'] = 'Zarážka bodu';
 $string['itemcomplete'] = 'Dokončiť';
 $string['items'] = 'Položky kontrolného zoznamu';
 
+$string['linktomodule'] = 'Odkaz na tento modul';
+
 $string['modulename'] = 'Kontrolný zoznam';
-$string['modulenameplural'] = 'Kontrolný zoznamy';
+$string['modulenameplural'] = 'Kontrolné zoznamy';
 
 $string['moveitemdown'] = 'Posunúť položku dole';
 $string['moveitemup'] = 'Posunúť položku hore';
@@ -64,15 +77,17 @@ $string['noitems'] = 'Žiadne položky v zozname';
 
 $string['optionalitem'] = 'Táto položka je nepovinná';
 $string['optionalhide'] = 'Skryť nepovinné položky';
-$string['optionalshow'] = 'Zobraziť nepovinnú položky';
+$string['optionalshow'] = 'Zobraziť nepovinné položky';
 
-$string['percentcomplete'] = 'Povinnú položky';
+$string['percentcomplete'] = 'Povinné položky';
 $string['percentcompleteall'] = 'Všetky položky';
 $string['preview'] = 'Prezrieť';
 $string['progress'] = 'Výkon';
 
 $string['report'] = 'Zobraziť výkon';
-$string['reporttablesummary'] = 'Tabuľka zobrazuje položky, ktoré každý splnil';
+$string['reporttablesummary'] = 'Tabuľka zobrazuje položky, ktoré každý účastník splnil';
+
+$string['removeauto'] = 'Odstrániť položky na moduly kurzu';
 
 $string['requireditem'] = 'Táto položka je vyžadovaná';
 
@@ -97,17 +112,22 @@ $string['teacheralongsidecheck'] = 'Učitelia a študenti';
 
 $string['theme'] = 'Téma zobrazenia kontrolného zoznamu';
 
+$string['updatecompletescore'] = 'Uložiť  známky/hodnotenia za dokončenie';
 $string['unindentitem'] = 'Zväčšiť odsadenie';
 $string['updateitem'] = 'Aktualizuj';
 $string['useritemsallowed'] = 'Užívateľ môže pridávať vlastné položky';
 $string['useritemsdeleted'] = 'Užívateľské položky vymazané';
 
 $string['view'] = 'Zobraziť zoznam';
-$string['viewall'] = 'Zobraziť pokrok všetky';
+$string['viewall'] = 'Zobraziť pokrok všetkých';
 $string['viewallcancel'] = 'Zrušiť';
 $string['viewallsave'] = 'Uložiť';
 
 $string['viewsinglereport'] = 'Zobraziť pokrok tohto užívateľa';
 $string['viewsingleupdate'] = 'Aktualizácia pokroku tohto užívateľa';
+
+
+$string['yesnooverride'] = 'Áno, neprepísať';
+$string['yesoverride'] = 'Áno, môže prepísať';
 
 ?>
