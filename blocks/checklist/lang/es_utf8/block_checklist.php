982faa76da448e39bda3a36fb8dbce95de5d430a
Davo Smith <git@davosmith.co.uk>
2011-08-27 18:03:18 +0100

    Updated Spanish translation provided by Fernando Sánchez

---
+++ blocks/checklist/lang/es_utf8/block_checklist.php
@@ -0,0 +1,9 @@
+<?php
+
+$string['checklist'] = 'Lista de Cotejo';
+$string['choosechecklist'] = 'Elija Lista de Cotejo';
+$string['choosegroup'] = 'Elija grupo';
+$string['nochecklist'] = 'Por favor edite este bloque para seleccionar una lista de verificaci&oacute;n para mostrar';
+$string['nochecklistplugin'] = 'Necesita instalar la &uacute;ltima versi&oacute;n del plugin de lista de control para este bloque de trabajo';
+$string['nousers'] = 'Sin usuarios';
+
