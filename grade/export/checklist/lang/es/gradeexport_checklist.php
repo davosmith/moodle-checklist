696c2e40a3f29cfb020afe1f4c104976fa2605c1
Davo Smith <git@davosmith.co.uk>
2011-08-27 18:13:31 +0100

    Updated Spanish translation provided by Fernando Sánchez

---
+++ grade/export/checklist/lang/es/gradeexport_checklist.php
@@ -0,0 +1,25 @@
+<?php
+
+$string['modulename'] = 'Exportaci&oacute;n Lista de Cotejo';
+
+$string['alldistrict'] = 'Todo';
+
+$string['choosechecklist'] = 'Lista de Cotejo para la exportaci&oacute;n';
+$string['choosedistrict'] = 'Distrito para incluir';
+
+$string['checklist:view'] = 'Ver informe de verificaci&oacute;n (Excel)';
+$string['checklist:viewall'] = 'Ver informe Lista de cotejo para todos los estudiantes en el curso ';
+$string['checklist:viewdistrict'] = 'Ver informe Lista de verificaci&oacute;n para los estudiantes de un mismo distrito';
+
+$string['checklistnotfound'] = 'Lista de Cotejo no encontrada';
+$string['checklistreport'] = 'Reporte';
+
+$string['export'] = 'Exportar archivo Excel ';
+
+$string['nochecklists'] = 'No hay listas de Cotejo adecuados';
+$string['nodistrict'] = 'Error: usuario actual no tiene ning&uacute;n distrito';
+$string['nousers'] = 'No hay los usuarios que exportar';
+
+$string['wrongdistrict'] = 'Usted no tiene permiso para ver los informes de ese distrito';
+
+
