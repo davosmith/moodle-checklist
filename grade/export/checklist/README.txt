4b00d421401b00501d379b03e0ba2030875ad47d
Davo Smith <git@davosmith.co.uk>
2011-12-03 18:05:59 +0000

    Grade export - added README and updated version.php

---
+++ grade/export/checklist/README.txt
@@ -0,0 +1,5 @@
+This is a grade export plugin which will create an Excel spreadsheet containing all the checkmarks from a single checklist.
+You can customise the included fields by editing the included file 'columns.php'.
+
+It will not work without the checklist module - which can be downloaded here: http://moodle.org/plugins/view.php?plugin=mod_checklist
+
--- grade/export/checklist/version.php
+++ grade/export/checklist/version.php
@@ -24,4 +24,9 @@
 ///////////////////////////////////////////////////////////////////////////
 
 $plugin->version  = 2011022402;
-$plugin->requires = 2010090501;
+$plugin->cron     = 0;
+$plugin->requires = 2010112400; // Moodle 2.0+
+$plugin->release  = '2.x (Build: 2011022402)';
+$plugin->maturity = MATURITY_STABLE;
+$plugin->dependencies = array('mod_checklist' => 2010041800); // Must have checklist activity module installed
+
