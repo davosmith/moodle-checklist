c677817eaad313d71f289204d036f993bccdf26c
Davo Smith <git@davosmith.co.uk>
2011-12-03 17:06:22 +0000

    Updated block version.php and added README

---
+++ blocks/checklist/README.txt
@@ -0,0 +1,4 @@
+This is a block which works with the checklist module and displays progress bars for a single checklist.
+
+It will not work without the checklist module - which can be downloaded here: http://moodle.org/plugins/view.php?plugin=mod_checklist
+
--- blocks/checklist/version.php
+++ blocks/checklist/version.php
@@ -1,3 +1,8 @@
 <?php
 
-$plugin->version = 2011040900;
+$plugin->version  = 2011040900;
+$plugin->cron     = 0;
+$plugin->requires = 2010112400; // Moodle 2.0+
+$plugin->release  = '2.x (Build: 2011040900)';
+$plugin->maturity = MATURITY_STABLE;
+$plugin->dependencies = array('mod_checklist' => 2010041800); // Must have checklist activity module installed
