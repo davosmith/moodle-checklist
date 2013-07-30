f43870504e63770083fc94b5ff4c036b3e4f5e78
Davo Smith <git@davosmith.co.uk>
2013-07-30 06:56:41 +0100

    Updated README/version

--- version.php
+++ version.php
@@ -25,9 +25,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$module->version  = 2013052200;  // The current module version (Date: YYYYMMDDXX)
+$module->version  = 2013073000;  // The current module version (Date: YYYYMMDDXX)
 $module->cron     = 60;          // Period for cron to check this module (secs)
 $module->maturity = MATURITY_STABLE;
-$module->release  = '2.x (Build: 2013052200)';
+$module->release  = '2.x (Build: 2013073000)';
 $module->requires = 2010112400;
 $module->component = 'mod_checklist';
