18f54ac01280b47bd1085d363d7c1fc07df271c3
Simon <s.bertoli@cfa.vic.gov.au>
2014-01-17 11:51:41 +1100

    Update to button.js
    Buttons now toggling through 'yes, no and null'

--- yui/buttons/buttons.js
+++ yui/buttons/buttons.js
@@ -1,48 +1,56 @@
 YUI.add('moodle-mod_checklist-buttons', function(Y) {
-   
-  var col_clicked = function(e)
-  {
-    // select the added hidden value denoting the row in question      
+
+  var col_clicked = function(e) {
+    // use the 1st value in the toggle array to toggle through states
+    col_toggle_id = column_toggle_values.shift();
+    // select the added hidden value denoting the row in question
     var id = this.getAttribute("id");
     // loop through all the select elements in the column
     Y.all('select[name*="['+id+']"').each(function () {
-        // change value of option one to selected and remove it from option 0
-        this.all('option').each(function () { 
-            if (this.getAttribute("value") == "0") { 
+        // loop through all the option tags in the select dropdown
+        this.all('option').each(function () {
+            if (this.getAttribute("value") != col_toggle_id) {
+                // remove the selected attribute from all option tags that are NOT the toggle value
                 this.removeAttribute("selected"); 
-            } else if (this.getAttribute("value") == "1") { 
-                this.setAttribute("selected", "selected"); 
-            }        
+            } else {
+                this.setAttribute("selected", "selected");
+            }
         });
     });
+    // place removed toggle value at the end of the toggle array
+    column_toggle_values.push(col_toggle_id);
   };
-  
-  
-  var row_clicked = function(e)
-  {
+
+  var row_clicked = function(e) {
+    // use the 1st value in the toggle array to toggle through states
+    row_toggle_id = row_toggle_values.shift();
     // select the added hidden value denoting the row in question
-    var id = this.next().getAttribute("value");
+    var id = this.getAttribute("id");
     // loop through all the select elements in the row
     Y.all('select[name*="items_'+id+'"').each(function () {
-        // change value of option one to selected and remove it from option 0
+        // loop through all the option tags in the select dropdown
         this.all('option').each(function () { 
-            if (this.getAttribute("value") == "0") { 
-                this.removeAttribute("selected"); 
-            } else if (this.getAttribute("value") == "1") { 
-                this.setAttribute("selected", "selected"); 
-            }        
+            if (this.getAttribute("value") != row_toggle_id) {
+                // remove the selected attribute from all option tags that are NOT the toggle value
+                this.removeAttribute("selected");
+            } else {
+                this.setAttribute("selected", "selected");
+            }
         });
     });
-  };  
-  
-  // simple click handlers for every added button
-  M.mod_checklist = M.mod_checklist || {};
-  M.mod_checklist.buttons = {
-    init: function() { 
-      //Y.one(".make_col_c").delegate("click", col_clicked);  
-      //Y.delegate("click", col_clicked, ".make_col_c");  
-	  Y.on("click", col_clicked, ".make_col_c");  
-     Y.on("click", row_clicked, ".make_c");
-	}
+    // place removed toggle value at the end of the toggle array    
+    row_toggle_values.push(row_toggle_id);
   };
+
+    // simple click handlers for every added button
+
+    M.mod_checklist = M.mod_checklist || {};
+    M.mod_checklist.buttons = {
+        init: function() {
+            Y.on("click", col_clicked, ".make_col_c");
+            Y.on("click", row_clicked, ".make_c");
+            column_toggle_values = Array(0, 1, 2);
+            row_toggle_values = Array(1, 2, 0);
+        }
+    };
 });
