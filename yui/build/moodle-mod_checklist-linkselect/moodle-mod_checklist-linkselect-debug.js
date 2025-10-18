abb7efd87a08595fb9a54a1d15a83d9cf1d8a60a
Davo Smith <davo.smith@synergy-learning.com>
2025-10-18 10:00:53 +0100

    Update stale YUI JS

--- yui/build/moodle-mod_checklist-linkselect/moodle-mod_checklist-linkselect-debug.js
+++ yui/build/moodle-mod_checklist-linkselect/moodle-mod_checklist-linkselect-debug.js
@@ -1,51 +1,52 @@
 YUI.add('moodle-mod_checklist-linkselect', function (Y, NAME) {
 
-    var SELECTOR = {
-        COURSELINK: '#menulinkcourseid',
-        URLLINK: '#id_linkurl',
-        OPENLINKINNEWWINDOW: '#id_openlinkinnewwindow'
-    };
+var SELECTOR = {
+    COURSELINK: '#menulinkcourseid',
+    URLLINK: '#id_linkurl',
+    OPENLINKINNEWWINDOW: '#id_openlinkinnewwindow'
+};
 
-    M.modChecklist = M.modChecklist || {};
-    M.modChecklist.linkselect = {
-        courselink: null,
-        urllink: null,
+M.modChecklist = M.modChecklist || {};
+M.modChecklist.linkselect = {
+    courselink: null,
+    urllink: null,
 
-        init: function () {
-            this.courselink = Y.one(SELECTOR.COURSELINK);
-            this.urllink = Y.one(SELECTOR.URLLINK);
-            this.openurlinnewwindow = Y.one(SELECTOR.OPENLINKINNEWWINDOW);
+    init: function() {
+        this.courselink = Y.one(SELECTOR.COURSELINK);
+        this.urllink = Y.one(SELECTOR.URLLINK);
+        this.openurlinnewwindow = Y.one(SELECTOR.OPENLINKINNEWWINDOW);
 
-            if (!this.courselink || !this.urllink) {
-                return; // If they're not both present, then there is nothing to do.
-            }
-
-            this.courselink.on('valuechange', this.fieldChanged, this);
-            this.urllink.on('valuechange', this.fieldChanged, this);
-            this.fieldChanged();
-        },
-
-        fieldChanged: function () {
-            var courseVal, urlVal;
-
-            courseVal = this.courselink.get('value');
-            urlVal = this.urllink.get('value');
-
-            if (courseVal) {
-                this.urllink.set('value', '');
-                this.urllink.set('disabled', true);
-                this.openurlinnewwindow.set('disabled', true);
-                this.openurlinnewwindow.set('checked', false);
-            } else if (urlVal) {
-                this.courselink.set('value', null);
-                this.courselink.set('disabled', true);
-                this.openurlinnewwindow.set('disabled', false);
-            } else {
-                this.urllink.set('disabled', false);
-                this.courselink.set('disabled', false);
-                this.openurlinnewwindow.set('disabled', false);
-            }
+        if (!this.courselink || !this.urllink) {
+            return; // If they're not both present, then there is nothing to do.
         }
-    };
+
+        this.courselink.on('valuechange', this.fieldChanged, this);
+        this.urllink.on('valuechange', this.fieldChanged, this);
+        this.fieldChanged();
+    },
+
+    fieldChanged: function() {
+        var courseVal, urlVal;
+
+        courseVal = this.courselink.get('value');
+        urlVal = this.urllink.get('value');
+
+        if (courseVal) {
+            this.urllink.set('value', '');
+            this.urllink.set('disabled', true);
+            this.openurlinnewwindow.set('disabled', true);
+            this.openurlinnewwindow.set('checked', false);
+        } else if (urlVal) {
+            this.courselink.set('value', null);
+            this.courselink.set('disabled', true);
+            this.openurlinnewwindow.set('disabled', false);
+        } else {
+            this.urllink.set('disabled', false);
+            this.courselink.set('disabled', false);
+            this.openurlinnewwindow.set('disabled', false);
+        }
+    }
+};
+
 
 }, '@VERSION@', {"requires": ["node", "event-valuechange"]});
