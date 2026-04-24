71103acd799061939d21c5c4012cb97489a3f550
Davo Smith <davo.smith@synergy-learning.com>
2026-04-24 15:33:35 +0100

    Update README and version

--- version.php
+++ version.php
@@ -24,9 +24,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2025101800;  // The current module version (Date: YYYYMMDDXX).
+$plugin->version = 2026042400;  // The current module version (Date: YYYYMMDDXX).
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '4.1.0.7';
+$plugin->release = '4.1.0.8';
 $plugin->requires = 2022112800; // Moodle 4.1.0.
 $plugin->component = 'mod_checklist';
-$plugin->supported = [401, 501];
+$plugin->supported = [401, 502];
